73c733ced437bd02a79ca5fe3dfd669441039ec4
klamerja <klamerja>
2023-04-04 16:23:04 +0200

    add matrice trasposta

--- Esercizi/Laboratori/Lab4/src/es1_1/matrice_trasposta.c
+++ Esercizi/Laboratori/Lab4/src/es1_1/matrice_trasposta.c
@@ -14,8 +14,10 @@
 *
 */
 
-int main()
-{
+//Prototipo
+void trasponi1_matrice(int *mat1, int *matRisultato, int righe, int colonne)
+
+int main(){
     // Per testare più velocemente il vostro algoritmo potete 
     // usare la matrice sotto (modificandola secondo le vostre necessità)
     // invece di leggerla da input con scanf. Quando siete pronti a
@@ -34,10 +36,24 @@ int main()
     leggi_array(*mat1, righe*colonne);
 
     // trasponi matrice
+    int matRisultato[colonne][righe];
+    trasponi1_matrice(*mat1, *matRisultato, righe, colonne);
 
     //stampiamo il risultato
     //funzione libreria
     stampa_matrice(*matRisultato, colonne, righe);
 
     return 0;
+}
+
+/**
+ * PRE: matrice generica
+ * POST: Trasposizione della matrice
+*/
+void trasponi1_matrice(int *mat1, int *matRisultato, int righe, int colonne){
+    for(int i=0;i<righe;i++){
+        for(int j=0;j<colonne;j++){
+            *(matRisultato+righe*j+i)=*(mat1+i*colonne+j);
+        }
+    }
 }
